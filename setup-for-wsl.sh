2ec33ba4caa1d91863f433d202c589e5befeb0fb
larry <JLawrenceB1@gmail.com>
2021-04-20 17:07:50 -0700

    Added terraform

--- setup-for-wsl.sh
+++ setup-for-wsl.sh
@@ -35,11 +35,14 @@ packages="
     mysql-client
     postgresql-client
     terminator
+    terraform
     tree
 "
 echo "======================================="
 echo "Installing linux packages"
 echo "======================================="
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update
 for package in $packages; do
     sudo apt-get -y install $package
